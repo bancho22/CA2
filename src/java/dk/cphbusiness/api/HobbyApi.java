198dfaf4fde5715d66fa6fb9ccee961fef918393
biggiejr <jrmacej@gmail.com>
2015-10-07 13:50:25 +0200

    Test

---
+++ src/java/dk/cphbusiness/api/HobbyApi.java
@@ -0,0 +1,75 @@
+package dk.cphbusiness.api;
+
+import dk.cphbusiness.facade.HobbyFacade;
+import javax.persistence.Persistence;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.UriInfo;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PUT;
+import javax.ws.rs.core.MediaType;
+import com.google.gson.*;
+import dk.cphbusiness.entity.Hobby;
+
+/**
+ * REST Web Service
+ *
+ * @author Mato
+ */
+@Path("hobby")
+public class HobbyApi {
+
+    @Context
+    private UriInfo context;
+
+    HobbyFacade facade = new HobbyFacade(Persistence.createEntityManagerFactory("CA2PU"));
+    private static Gson gson = new GsonBuilder().setPrettyPrinting().setFieldNamingPolicy(FieldNamingPolicy.IDENTITY).create();
+
+    /**
+     * Creates a new instance of GenericResource
+     */
+    public HobbyApi() {
+    }
+
+    /**
+     * Retrieves representation of an instance of
+     * dk.cphbusiness.api.GenericResource
+     *
+     * @return an instance of java.lang.String
+     */
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public String getHobby(@PathParam("id") int id) {
+        return gson.toJson(facade.getHobby(id));
+    }
+
+    /**
+     * PUT method for updating or creating an instance of GenericResource
+     *
+     * @param content representation for the resource
+     * @return an HTTP response with content of the updated or created resource.
+     */
+    @PUT
+    @Produces("application/json")
+    @Consumes("application/json")
+    public String putJson(String name, String description) {
+        Hobby h = new Hobby();
+        h.setName(name);
+        h.setDescription(description);
+        h = facade.editHobby(h);
+        return gson.toJson(facade.editHobby(h));
+    }
+
+    @DELETE
+    @Consumes("application/json")
+    public String deleteJson(@PathParam("id") int id) {
+        facade.deleteHobby(id);
+        return gson.toJson(facade.deleteHobby(id));
+    }
+
+}
