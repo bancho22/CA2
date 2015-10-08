98ee301d3966b57eb3d79e5e8402a881e4feb067
biggiejr <jrmacej@gmail.com>
2015-10-08 13:58:33 +0200

    js working with first point

--- web/js/main.js
+++ web/js/main.js
@@ -11,14 +11,14 @@ function findPerson() {
         type: 'GET',
         url: "api/person/byphone/" + $("#search").val(),
         success: function (person) {
+            $("#table").html("");
+            $("#table").html("<tr><th>First Name</th>  <th>Last Name</th>  <th>Phone</th>  <th>Adress</th></tr>");
             $("#table").append('<tr><td>' +
                     person.firstName + '</td><td>' +
-                    person.lastName + '</td><td>' 
-//                    $.each(person.phones, function (phone) {
-//                        phone.number;
-//                    }
-//
-//                    )
+                    person.lastName + '</td><td>' +
+                    person.address.street + '</td><td>'
+                    
+
                     );
 
         }
